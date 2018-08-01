4f0026f58e8dc4da0deb86340090011c7bf65582
Sargy Lukina <lukina_sargy@mail.ru>
2018-08-02 00:08:30 +0300

    footer

--- src/js/slider.js
+++ src/js/slider.js
@@ -16,8 +16,28 @@ $(document).ready(function(){
 	      	centerMode: false,
 	      	slidesToShow: 3,
 	  		slidesToScroll: 1,
-	  		scrollbar:true,
 	  		arrows:false,
+	      }
+	    }
+	  ]
+	});
+	$('.footer-slider').slick({
+	  mobileFirst: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  centerMode: true,
+	  swipeToSlide: true,
+	  variableWidth:true,
+	  arrows: false,
+	  dots: false,
+	  infinite: true,
+	  responsive: [
+	    {
+	      breakpoint: 1910,
+	      settings: {
+	      	slidesToShow: 1,
+	  		slidesToScroll: 1,
+	  		arrows:true,
 	      }
 	    }
 	  ]
